d4388fc1a156fd16bfb012b93de3ee3b85f9bc5e
JulienKisoni <kisonijulien@gmail.com>
2020-03-17 13:57:17 +0100

    connected to db

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('DB Connected'))
+    .catch(e => console.log('DB not connected', e));
+
 app.use(bodyParser.json());
+app.use('/graphql', expressGraphQL({
+    graphiql: true
+}));
 
 app.listen(4000, () => {
     console.log('app listening on port 4000');
